daa3acdf52ea256cf4e6a7aa29c0648ce66ac11e
Julio Colon <167486401+S3544988@users.noreply.github.com>
2026-03-30 11:59:15 -0400

    Update InvalidBirdDataException.java

--- edu/fscj/cop2800c/penguin/InvalidBirdDataException.java
+++ edu/fscj/cop2800c/penguin/InvalidBirdDataException.java
@@ -1,1 +1,12 @@
+//InvalidBirdDataException.java
+// Julio Colon
+// 3/30/26
+// Custom exception for invalid bird data
 
+package edu.fscj.cop2800c.penguin; // use your project’s package
+
+public class InvalidBirdDataException extends Exception {
+    public InvalidBirdDataException(String message) {
+        super(message);
+    }
+}
